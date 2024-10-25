1909bc9f54755621a45de23c10a8b607d503fcb8
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2024-10-25 09:07:02 -0300

    Funcionalidades, melhor resolução possivel, exibir titulo do video, printar Download Completo

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-from pytube import
+from pytubefix import YouTube
 
-yt =
+# Solicitar a URL do vídeo
+url = input('Cole aqui sua url: ')
+yt = YouTube(url)
+
+# Exibir título e thumbnail do vídeo
+print("Título:", yt.title)
+print("Thumbnail URL:", yt.thumbnail_url)
+
+# Obter streams disponíveis
+streams = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc()
+
+# Exibir opções de resolução
+print("Resuloções disponíveis:")
+for stream in streams:
+    print(stream.resolution)
+
+# Selecionar a melhor resolução
+best_stream = streams.first() # Pega o primeiro stream da lista, que é o de melhor resolução
+print(f"Baixando: {best_stream.resolution}")
+
+# Fazer o download do vídeo
+best_stream.download()
+print("Download completo!")
